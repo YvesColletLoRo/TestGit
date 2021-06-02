59473d50a4516fb40011624aebab25cc5134f37e
Yves.Collet <yves.collet@loro.ch>
2021-06-02 08:57:46 +0200

    changement texte

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("coucou");
+            Console.WriteLine("salut");
         }
     }
 }
